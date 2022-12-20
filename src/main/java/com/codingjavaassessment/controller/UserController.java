d1fd4038b1175b03ac655b01324be1a80526b6f8
FranQuintelaVela <fraquivel@gmail.com>
2022-12-20 12:36:10 +0100

    Updated cors policy

--- src/main/java/com/codingjavaassessment/controller/UserController.java
+++ src/main/java/com/codingjavaassessment/controller/UserController.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000", "https://gentle-water-0252d5b03.2.azurestaticapps.net")
 @RestController
 @RequestMapping("/api")
 public class UserController {
